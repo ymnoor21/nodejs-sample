c712c5f458d0bef9ffbf86173f763e230473b7c5
Yamin Noor <ymnoor21@gmail.com>
2015-11-29 19:14:40 -0800

    Unit test for invalid flight number

--- test/FlightSpec.js
+++ test/FlightSpec.js
@@ -5,7 +5,7 @@ var mocha = require('mocha'),
 	should = require('should');
 
 describe('flights', function() {
-	it ('should return valid data for flight 237', function(done) {
+	it ('should return valid data for flight 237', function (done) {
 		supertest(app)
 		.get('/flights/237')
 		.expect(200)
@@ -13,5 +13,15 @@ describe('flights', function() {
 			should(res.status).be.exactly(200).and.be.a.Number();
 			done();
 		});
+	});
+
+	it ('should return an error for invalid flight number', function (done) {
+		supertest(app)
+		.get('/flights/400')
+		.expect(404)
+		.end(function (err, res) {
+			should(res.status).be.exactly(404).and.be.a.Number();
+			done();
+		})
 	});
 });
