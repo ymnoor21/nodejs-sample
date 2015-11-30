77c928f645df3b0fe4be2f3f61ab64ae02ae879c
Yamin Noor <ymnoor21@gmail.com>
2015-11-29 17:34:21 -0800

    Initial commit

---
+++ data/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+	"1": {
+		number: 1,
+		origin: 'LAX',
+		destination: 'DCA',
+		departs: '5:00 PM',
+		arrives: '12:00 AM'
+	},
+	"433": {
+		number: 433,
+		origin: 'SMF',
+		destination: 'LAX',
+		departs: '3:00 PM',
+		arrives: '4:30 PM'
+	}
+};
---
+++ routes/flights.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+var flight = require('../flight');
+var flights = require('../data');
+
+for (var number in flights) {
+	flights[number] = flight.create(flights[number])
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('Flight Index', { title: 'Express' });
+});
+
+router.get('/list', function(req, res, next) {
+  	res.render('list', { title: 'Flights List', flights: flights});
+});
+
+router.get('/list/json', function(req, res, next) {
+	var flightData = [];
+
+	for (var number in flights) {
+		flightData.push(flights[number].getInformation());
+	}
+
+	res.json(flightData);
+});
+
+router.get('/:number', function(req, res, next) {
+	var number = req.param('number');
+  	if (typeof flights[number] === 'undefined') {
+  		res.status(404).json({'status': 'error'});
+  	} else {
+  		res.json(flights[number].getInformation());
+  	}
+});
+
+router.put('/:number/arrived', function(req, res, next) {
+	var number = req.param('number');
+  	if (typeof flights[number] === 'undefined') {
+  		res.status(404).json({'status': 'error'});
+  	} else {
+  		flights[number].triggerArrive();
+  		res.json({'status': 'done', 'info': flights[number].getInformation()});
+  	}
+});
+
+module.exports = router;
