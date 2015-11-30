3aa220c1772987821c5046d1f1b06405085cdadb
Yamin Noor <ymnoor21@gmail.com>
2015-11-29 18:06:54 -0800

    Decoupled app, flights and data

--- data/index.js
+++ data/index.js
@@ -1,16 +1,23 @@
 module.exports = {
-	"1": {
-		number: 1,
-		origin: 'LAX',
-		destination: 'DCA',
-		departs: '5:00 PM',
-		arrives: '12:00 AM'
-	},
-	"433": {
-		number: 433,
-		origin: 'SMF',
-		destination: 'LAX',
-		departs: '3:00 PM',
-		arrives: '4:30 PM'
-	}
+   "1": {
+      number: 1,
+      origin: 'LAX',
+      destination: 'DCA',
+      departs: '5:00 PM',
+      arrives: '12:00 AM'
+   },
+   "237": {
+      number: 237,
+      origin: 'JFK',
+      destination: 'DCA',
+      departs: '9:00 AM',
+      arrives: '11:30 AM'
+   },
+   "433": {
+      number: 433,
+      origin: 'SMF',
+      destination: 'LAX',
+      departs: '3:00 PM',
+      arrives: '4:30 PM'
+   }
 };
